c962d119deaaae7870c54424e6c5223d0c83159c
Hanul <hanul@hanul.me>
2017-12-09 16:45:02 +0900

    init

---
+++ app/src/main/java/co/hanul/downloadandplaysound/DownloadAndPlaySound.java
@@ -0,0 +1,155 @@
+package co.hanul.downloadandplaysound;
+
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DownloadAndPlaySound {
+
+    private String tag;
+    private URL url;
+    private String filename;
+
+    private String folderPath;
+    private String path;
+
+    private boolean isReady;
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+    private int currentPosition;
+
+    public DownloadAndPlaySound(String tag, String url, String filename) {
+
+        this.tag = tag;
+        try {
+            this.url = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        this.filename = filename;
+
+        folderPath = Environment.getExternalStorageDirectory() + "/" + tag + "/gamesound/";
+        path = folderPath + filename;
+
+        // 이미 파일이 존재하면 즉시 준비 완료
+        if (new File(path).exists() == true) {
+            ready();
+        }
+
+        // 아니면 다운로드 시작
+        else {
+            File folder = new File(folderPath);
+            if (folder.exists() != true) {
+                folder.mkdirs();
+            }
+            new DownloadTask().execute();
+        }
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                release();
+            }
+        });
+    }
+
+    public void release() {
+
+        if (mediaPlayer != null) {
+
+            if (mediaPlayer.isPlaying() == true) {
+                mediaPlayer.stop();
+            }
+
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    public void pause() {
+        if (mediaPlayer != null && isReady == true) {
+            mediaPlayer.pause();
+            currentPosition = mediaPlayer.getCurrentPosition();
+        }
+    }
+
+    public void resume() {
+        if (mediaPlayer != null && isReady == true) {
+            mediaPlayer.seekTo(currentPosition);
+            mediaPlayer.start();
+        }
+    }
+
+    private void ready() {
+        isReady = true;
+
+        try {
+            mediaPlayer.setDataSource(path);
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mediaPlayer.start();
+    }
+
+    private class DownloadTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... notUsing) {
+            InputStream input = null;
+            OutputStream output = null;
+            HttpURLConnection connection = null;
+            try {
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+
+                    input = connection.getInputStream();
+                    output = new FileOutputStream(path);
+
+                    byte data[] = new byte[4096];
+                    int count;
+                    while ((count = input.read(data)) != -1) {
+                        // Task가 중단되면 끝
+                        if (isCancelled() == true) {
+                            input.close();
+                            return null;
+                        }
+                        output.write(data, 0, count);
+                    }
+                    // 다운로드가 끝나면 준비 완료
+                    ready();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (output != null) {
+                        output.close();
+                    }
+                    if (input != null) {
+                        input.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/co/hanul/downloadandplaysound/MainActivity.java
@@ -0,0 +1,48 @@
+package co.hanul.downloadandplaysound;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    private DownloadAndPlaySound downloadAndPlaySound;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.play_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downloadAndPlaySound = new DownloadAndPlaySound("DownloadAndPlaySoundTest6", "http://cwserver3.btncafe.com:8523/R/gamesound/bgm_home.ogg", "bgm_home.ogg");
+            }
+        });
+
+        findViewById(R.id.stop_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downloadAndPlaySound.release();
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (downloadAndPlaySound != null) {
+            downloadAndPlaySound.pause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (downloadAndPlaySound != null) {
+            downloadAndPlaySound.resume();
+        }
+    }
+}
